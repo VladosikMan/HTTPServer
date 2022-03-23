4a416af74ad7a131d9f8067a8238d8cb27066ed2
Vladislav Vedernikov <vladved2424@gmail.com>
2022-03-23 15:18:44 +0300

    Update Constants

--- src/main/java/server/Constants.java
+++ src/main/java/server/Constants.java
@@ -2,4 +2,5 @@ package server;
 
 public class Constants {
     public final static String ALIAS = "serverHTTPS";
+    public final static char[] privateKeyPassword = ("passwordverywrll").toCharArray();
 }
