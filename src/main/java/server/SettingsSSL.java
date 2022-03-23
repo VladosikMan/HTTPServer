5a98982cb28e21810fc9051773f08cdc8ade5871
Vladislav Vedernikov <vladved2424@gmail.com>
2022-03-23 15:19:55 +0300

    start server with configure ssl

--- src/main/java/server/SettingsSSL.java
+++ src/main/java/server/SettingsSSL.java
@@ -1,13 +1,10 @@
 package server;
 
 
-import java.security.InvalidAlgorithmParameterException;
-import java.security.KeyStore;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
-import java.security.cert.X509Certificate;
-
 import nl.altindag.ssl.SSLFactory;
+
+import java.security.PrivateKey;
+import java.security.cert.Certificate;
 
 
 public class SettingsSSL {
@@ -16,7 +13,23 @@ public class SettingsSSL {
     private SSLFactory sslFactory;
 
     public SettingsSSL() {
+    }
+
+    public void setConfigeSSL() {
+
+        //получить все необходимые дл€ ssl аутетификации сервера
+        Cryprography cryprography = new Cryprography();
+        cryprography.generatePair();
+        PrivateKey privateKey = cryprography.getPrivateKey();
+        Certificate certificate = cryprography.generateX509Certificate(cryprography.getKeyPair());
+
+//        System.out.println(privateKey);
+//
+//        System.out.println("--------------------------");
+//        System.out.println(certificate);
+
         sslFactory = SSLFactory.builder()
+
                 // опци€ гор€чего переключени€ материалов
                 .withSwappableIdentityMaterial()
                 .withSwappableTrustMaterial()
@@ -27,15 +40,12 @@ public class SettingsSSL {
                 .withProtocols("TLSv1.3", "TLSv1.2")
                 .withCiphers("TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384", "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384")
 
+                .withIdentityMaterial(privateKey, Constants.privateKeyPassword, certificate)
                 //.withIdentityMaterial(privateKey, privateKeyPassword, certificateChain)
                 //.withSessionTimeout(3600) // Amount of seconds until it will be invalidated
                 //.withSessionCacheSize(1024) // Amount of bytes until it will be invalidated
 
                 .build();
-    }
-
-    public void setConfigeSSL() {
-        //получить все необходимые дл€ ssl аутетификации сервера
 
 
     }
