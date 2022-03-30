d4653e71c695f736b4f424b79e257dbca6709042
Vladislav Vedernikov <vladved2424@gmail.com>
2022-03-30 14:16:14 +0300

    add paths for files ident materials

--- src/main/java/server/Constants.java
+++ src/main/java/server/Constants.java
@@ -2,5 +2,11 @@ package server;
 
 public class Constants {
     public final static String ALIAS = "serverHTTPS";
-    public final static char[] privateKeyPassword = ("passwordverywrll").toCharArray();
+    public final static char[] privateKeyPassword = ("1234").toCharArray();
+
+
+
+    //------------------------Paths--------
+    public final static String PATH_CERT = "cert/localhost.crt";
+    public final static String PATH_PRIVATE_KEY = "cert/localhost.pem";
 }
