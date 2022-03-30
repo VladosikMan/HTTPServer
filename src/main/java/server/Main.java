9d4e9f2f5a5895e0d74b27168fb3dc6382e7123e
Vladislav Vedernikov <vladved2424@gmail.com>
2022-03-30 14:16:51 +0300

    Main : class run app

--- src/main/java/server/Main.java
+++ src/main/java/server/Main.java
@@ -1,2 +1,21 @@
-package server;public class Main {
+package server;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.spec.InvalidKeySpecException;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // http://localhost:18443/
+        ServerHTTPD serverHTTPD = new ServerHTTPD(8443);
+        serverHTTPD.startServer();
+
+    }
 }
