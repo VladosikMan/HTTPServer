24a58b9b4d5c8a72a1b1dcac5f753e997ace6716
Vladislav Vedernikov <vladved2424@gmail.com>
2022-03-30 14:15:23 +0300

    ssl : update ident material from files

--- src/main/java/server/SettingsSSL.java
+++ src/main/java/server/SettingsSSL.java
@@ -3,6 +3,10 @@ package server;
 
 import nl.altindag.ssl.SSLFactory;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.security.PrivateKey;
 import java.security.cert.Certificate;
 
@@ -15,33 +19,32 @@ public class SettingsSSL {
     public SettingsSSL() {
     }
 
-    public void setConfigeSSL() {
+    public void setConfigeSSL() throws Exception {
 
         //получить все необходимые дл€ ssl аутетификации сервера
         Cryprography cryprography = new Cryprography();
-        cryprography.generatePair();
-        PrivateKey privateKey = cryprography.getPrivateKey();
-        Certificate certificate = cryprography.generateX509Certificate(cryprography.getKeyPair());
+//        cryprography.generatePair();
+//        PrivateKey privateKey = cryprography.getPrivateKey();
+//        Certificate certificate = cryprography.generateX509Certificate(cryprography.getKeyPair());
 
-//        System.out.println(privateKey);
-//
-//        System.out.println("--------------------------");
-//        System.out.println(certificate);
-
+        InputStream targetStream = new FileInputStream(new File(Constants.PATH_CERT));
+        Certificate cert = cryprography.getCert(targetStream);
+        PrivateKey privatekey = cryprography.readPrivateKey(new File(Constants.PATH_PRIVATE_KEY));
         sslFactory = SSLFactory.builder()
 
                 // опци€ гор€чего переключени€ материалов
-                .withSwappableIdentityMaterial()
-                .withSwappableTrustMaterial()
+                //.withSwappableIdentityMaterial()
+                //.withSwappableTrustMaterial()
 
 
                 // сертификаты jdk и OS
-                .withDefaultTrustMaterial()
-                .withProtocols("TLSv1.3", "TLSv1.2")
-                .withCiphers("TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384", "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384")
+                //.withDefaultTrustMaterial()
+                //.withTrustMaterial(cert)
+                //.withProtocols("TLSv1.3", "TLSv1.2")
+                //.withCiphers("TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384", "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384")
 
-                .withIdentityMaterial(privateKey, Constants.privateKeyPassword, certificate)
-                //.withIdentityMaterial(privateKey, privateKeyPassword, certificateChain)
+                //.withDefaultIdentityMaterial()
+                .withIdentityMaterial(privatekey, Constants.privateKeyPassword, cert)
                 //.withSessionTimeout(3600) // Amount of seconds until it will be invalidated
                 //.withSessionCacheSize(1024) // Amount of bytes until it will be invalidated
 
